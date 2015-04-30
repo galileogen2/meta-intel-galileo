7036670c6da5b4419f8ef5372168d46f2727d347
Josef Ahmad <josef.ahmad@intel.com>
2015-04-30 19:32:10 +0800

    launcher.sh: start clloader only on galileo
    Add the dmidecode logic that comes from galileod.sh to launcher.sh.
    At the moment this is pure duplication.
    
    Signed-off-by: Josef Ahmad <josef.ahmad@intel.com>
    Reviewed-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Tested-by: Ong Boon Leong <boon.leong.ong@intel.com>
    Signed-off-by: Chang Rebecca Swee Fun <rebecca.swee.fun.chang@intel.com>
    Signed-off-by: Brendan Le Foll <brendan.le.foll@intel.com>

--- recipes-galileo/galileo-target/files/launcher.sh
+++ recipes-galileo/galileo-target/files/launcher.sh
@@ -11,11 +11,29 @@ mytrap()
 
 trap 'mytrap' USR1
 
-keepgoing=true
-while $keepgoing
-do
-  $CLLOADER $CLLOADER_OPTS < /dev/ttyGS0 > /dev/ttyGS0 & clPID=$!
-  wait $clPID
-  usleep 200000
-done
+arduino_services()
+{
+  keepgoing=true
+  while $keepgoing
+  do
+      $CLLOADER $CLLOADER_OPTS < /dev/ttyGS0 > /dev/ttyGS0 & clPID=$!
+      wait $clPID
+      usleep 200000
+  done
+}
 
+galileo_board=false
+type dmidecode > /dev/null 2>&1 || die "dmidecode not installed"
+board=$(dmidecode -s baseboard-product-name)
+case "$board" in
+    *"Galileo" )
+               galileo_board=true
+               ;;
+    *"GalileoGen2" )
+               galileo_board=true
+               ;;
+esac
+
+if [ $galileo_board == "true" ]; then
+    arduino_services
+fi
